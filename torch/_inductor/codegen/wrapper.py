568e6ce7c7eba8814c00ef86dbdcdc8c476473e2
Kefei Lu <kefeilu@meta.com>
2023-07-07 16:45:54 -0700

    AOT inductor: error: ‘c10::Dispatcher’ has not been declared (#104742)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/104742
    
    Reviewed By: desertfire
    
    Differential Revision: D47275262
    
    fbshipit-source-id: 705c157d04ccbbef24192dbc400adcc344aa80c1

--- torch/_inductor/codegen/wrapper.py
+++ torch/_inductor/codegen/wrapper.py
@@ -1253,6 +1253,7 @@ class CudaWrapperCodeGen(CppWrapperCodeGen):
         self.header.splice(
             """
             #include <ATen/native/BinaryOps.h>
+            #include <ATen/core/dispatch/Dispatcher.h>
             #include <c10/util/Exception.h>
             #include <c10/cuda/CUDAGuard.h>
 
